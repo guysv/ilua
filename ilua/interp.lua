8a7c4884475ee511805e683ecd97ff143afebeb8
guysv <sviryguy@gmail.com>
2018-09-26 16:30:12 +0300

    Relicense as GPLv2

--- ilua/interp.lua
+++ ilua/interp.lua
@@ -1,8 +1,8 @@
 -- ILua
 -- Copyright (C) 2018  guysv
 
--- This file is part of ILua which is released under GPLv3.
--- See file LICENSE or go to https://www.gnu.org/licenses/gpl-3.0.txt
+-- This file is part of ILua which is released under GPLv2.
+-- See file LICENSE or go to https://www.gnu.org/licenses/gpl-2.0.txt
 -- for full license details.
 
 local dynamic_env = {}
