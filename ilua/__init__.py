6626598066e4a816e485780bdf4e11e5a99ab878
guysv <sviryguy@gmail.com>
2018-09-26 16:05:17 +0300

    Add Copyright Notice

---
+++ ilua/__init__.py
@@ -0,0 +1,6 @@
+# ILua
+# Copyright (C) 2018  guysv
+
+# This file is part of ILua which is released under GPLv3.
+# See file LICENSE or go to https://www.gnu.org/licenses/gpl-3.0.txt
+# for full license details.
--- ilua/_win32namedpipe.py
+++ ilua/_win32namedpipe.py
@@ -1,3 +1,10 @@
+# ILua
+# Copyright (C) 2018  guysv
+
+# This file is part of ILua which is released under GPLv3.
+# See file LICENSE or go to https://www.gnu.org/licenses/gpl-3.0.txt
+# for full license details.
+
 import os
 import msvcrt
 import win32pipe
